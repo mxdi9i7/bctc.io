f24aa6c201aa0b8f9b02dff933b14a24e53bc9dc
mxdi9i7 <peter.zheng88228@gmail.com>
2017-08-01 17:22:05 -0400

    update

--- client/src/components/index/TopBox.js
+++ client/src/components/index/TopBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import $ from 'jquery'
 import '../../css/index/topBox.css'
+
 class TopBox extends Component {
     constructor(props) {
         super(props)
@@ -11,6 +12,13 @@ class TopBox extends Component {
   render() {
     return (
         <div className="topBox">
+            <div className="row">
+                <div className="col-md-6 col-sm-12">
+
+                </div>
+                <div className="col-md-6 col-sm-12">
+                </div>
+            </div>
         </div>   
     );
   }
